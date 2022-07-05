c5e2d8a3572220d6d5c8a0edd51c23d2bb106594
OrlandoSD <py2osd.orlando@gmail.com>
2022-07-05 11:57:34 -0300

    atualizando dados e finalizando a aula

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,18 +1,35 @@
-import express, {Response, Request, NextFunction} from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
-//Configurações da aplicação
-app.use(express.json);
-app.use(express.urlencoded({ extended:true}));
 
-// Configuração de rotas
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de Rotas
 app.use(statusRoute);
 app.use(usersRoute);
 
-
-
-// inicialização do servidor
+// Inicialização do servidor
 app.listen(3000, () => {
-    console.log('aplicação executendo na porta 3000!!!')
-})
+    console.log('Aplicação executando na porta 3000!');
+});import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
+
+const app = express();
+
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização do servidor
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
--- live-01/src/routes/users.route.ts
+++ live-01/src/routes/users.route.ts
@@ -1,39 +1,34 @@
-import { Router } from "express";
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-    const users = [{userName: 'Renam'}];
+    const users = [{ userName: 'Renan' }];
     res.status(StatusCodes.OK).send(users);
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    //bancoDeDados.getUserByUUid(uuid)
-
-    res.sendStatus(200);
+    res.status(StatusCodes.OK).send({ uuid });
 });
 
-
-usersRoute.get('/users', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-    //bancoDeDados.getUserByUUid(uuid)
     res.status(StatusCodes.CREATED).send(newUser);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction)=>{
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
 
     modifiedUser.uuid = uuid;
 
-    res.status(StatusCodes.OK).send({modifiedUser});
+    res.status(StatusCodes.OK).send(modifiedUser);
 });
 
-usersRoute.delete('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction)=>{
-    res.status(StatusCodes.OK);
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
