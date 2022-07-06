e850d0daa19f53164c4c25be9c3ce50f77d89cd1
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-07-06 17:56:17 -0300

    commit de validação de token

--- live-03/src/routes/authorization.route.ts
+++ live-03/src/routes/authorization.route.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import JWT from 'jsonwebtoken';
 import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
+import jwtAuthenticationMiddleware from '../middlewares/JWT-authentication.middleware';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 
 const authorizationRoute = Router();
@@ -24,6 +25,11 @@ authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Req
     } catch (error) {
         next(error);
     }
+});
+
+authorizationRoute.post('/token/validate', jwtAuthenticationMiddleware, (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+
 });
 
 
